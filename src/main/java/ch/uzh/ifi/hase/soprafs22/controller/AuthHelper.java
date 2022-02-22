59dedde7de72210e03e6435947895350ac2ab75c
BacLuc <lucius.bachmann@gmx.ch>
2022-02-22 19:38:35 +0100

    LoginController: extract AuthHelper
    Because we need to create a cookie for the user registration too.

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/AuthHelper.java
@@ -0,0 +1,32 @@
+package ch.uzh.ifi.hase.soprafs22.controller;
+
+import ch.uzh.ifi.hase.soprafs22.security.jwt.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AuthHelper {
+  private final AuthenticationManager authenticationManager;
+  private final JwtUtil jwtUtil;
+
+  @Autowired
+  public AuthHelper(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
+    this.authenticationManager = authenticationManager;
+    this.jwtUtil = jwtUtil;
+  }
+
+  public ResponseCookie createCookieFor(String username, String password) {
+    Authentication authentication =
+        authenticationManager.authenticate(
+            new UsernamePasswordAuthenticationToken(username, password));
+    SecurityContextHolder.getContext().setAuthentication(authentication);
+    UserDetails principal = (UserDetails) authentication.getPrincipal();
+    return jwtUtil.generateJwtCookie(principal);
+  }
+}
--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/LoginController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/LoginController.java
@@ -1,17 +1,11 @@
 package ch.uzh.ifi.hase.soprafs22.controller;
 
 import ch.uzh.ifi.hase.soprafs22.rest.dto.LoginDTO;
-import ch.uzh.ifi.hase.soprafs22.security.jwt.JwtUtil;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,24 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 public class LoginController {
-  private final AuthenticationManager authenticationManager;
-  private final JwtUtil jwtUtil;
+  private final AuthHelper authHelper;
 
   @Autowired
-  public LoginController(AuthenticationManager authenticationManager, JwtUtil jwtUtil) {
-    this.authenticationManager = authenticationManager;
-    this.jwtUtil = jwtUtil;
+  public LoginController(AuthHelper authHelper) {
+    this.authHelper = authHelper;
   }
 
   @PostMapping("/login")
   public ResponseEntity<Void> login(@Valid @RequestBody LoginDTO loginDTO) {
-    Authentication authentication =
-        authenticationManager.authenticate(
-            new UsernamePasswordAuthenticationToken(
-                loginDTO.getUsername(), loginDTO.getPassword()));
-    SecurityContextHolder.getContext().setAuthentication(authentication);
-    UserDetails principal = (UserDetails) authentication.getPrincipal();
-    ResponseCookie responseCookie = jwtUtil.generateJwtCookie(principal);
+    ResponseCookie responseCookie =
+        authHelper.createCookieFor(loginDTO.getUsername(), loginDTO.getPassword());
     return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, responseCookie.toString()).build();
   }
 }
